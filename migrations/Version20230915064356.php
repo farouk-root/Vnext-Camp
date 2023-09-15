fe67bba2e7a64ad639606c150f9c2de9f79dd423
farouk-root <farouk.chalghoumi@esprit.tn>
2023-09-15 07:45:16 +0100

    Create Service Entity

---
+++ migrations/Version20230915064356.php
@@ -0,0 +1,35 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20230915064356 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE TABLE service (id INT AUTO_INCREMENT NOT NULL, name VARCHAR(255) NOT NULL, type VARCHAR(255) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE teams (id INT AUTO_INCREMENT NOT NULL, travailler_id_id INT DEFAULT NULL, member_count INT DEFAULT NULL, INDEX IDX_96C22258C55B0D1 (travailler_id_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('ALTER TABLE teams ADD CONSTRAINT FK_96C22258C55B0D1 FOREIGN KEY (travailler_id_id) REFERENCES travailler (id)');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE teams DROP FOREIGN KEY FK_96C22258C55B0D1');
+        $this->addSql('DROP TABLE service');
+        $this->addSql('DROP TABLE teams');
+    }
+}
