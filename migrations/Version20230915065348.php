67a19af3a5c4131abb764b62e3c3f936865c8e0d
farouk-root <farouk.chalghoumi@esprit.tn>
2023-09-15 07:54:04 +0100

    created Donation Entity

---
+++ migrations/Version20230915065348.php
@@ -0,0 +1,43 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20230915065348 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE TABLE donation (id INT AUTO_INCREMENT NOT NULL, type VARCHAR(255) NOT NULL, amount INT DEFAULT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE ressource (id INT AUTO_INCREMENT NOT NULL, services_id INT DEFAULT NULL, type VARCHAR(255) NOT NULL, quantity INT DEFAULT NULL, localisation VARCHAR(255) DEFAULT NULL, INDEX IDX_939F4544AEF5A6C1 (services_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE service (id INT AUTO_INCREMENT NOT NULL, name VARCHAR(255) NOT NULL, type VARCHAR(255) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE teams (id INT AUTO_INCREMENT NOT NULL, travailler_id_id INT DEFAULT NULL, service_type_id INT DEFAULT NULL, member_count INT DEFAULT NULL, INDEX IDX_96C22258C55B0D1 (travailler_id_id), INDEX IDX_96C22258AC8DE0F (service_type_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('ALTER TABLE ressource ADD CONSTRAINT FK_939F4544AEF5A6C1 FOREIGN KEY (services_id) REFERENCES service (id)');
+        $this->addSql('ALTER TABLE teams ADD CONSTRAINT FK_96C22258C55B0D1 FOREIGN KEY (travailler_id_id) REFERENCES travailler (id)');
+        $this->addSql('ALTER TABLE teams ADD CONSTRAINT FK_96C22258AC8DE0F FOREIGN KEY (service_type_id) REFERENCES service (id)');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE ressource DROP FOREIGN KEY FK_939F4544AEF5A6C1');
+        $this->addSql('ALTER TABLE teams DROP FOREIGN KEY FK_96C22258C55B0D1');
+        $this->addSql('ALTER TABLE teams DROP FOREIGN KEY FK_96C22258AC8DE0F');
+        $this->addSql('DROP TABLE donation');
+        $this->addSql('DROP TABLE ressource');
+        $this->addSql('DROP TABLE service');
+        $this->addSql('DROP TABLE teams');
+    }
+}
---
+++ src/Entity/Donation.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\DonationRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: DonationRepository::class)]
+class Donation
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $Type = null;
+
+    #[ORM\Column(nullable: true)]
+    private ?int $Amount = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getType(): ?string
+    {
+        return $this->Type;
+    }
+
+    public function setType(string $Type): static
+    {
+        $this->Type = $Type;
+
+        return $this;
+    }
+
+    public function getAmount(): ?int
+    {
+        return $this->Amount;
+    }
+
+    public function setAmount(?int $Amount): static
+    {
+        $this->Amount = $Amount;
+
+        return $this;
+    }
+}
